931cbaa41b8b16ab2c005c5ca048f4a05db12bd7
NFHaq <nutanhaq@gmail.com>
2018-02-13 15:59:07 +0600

    added gitignore

--- style.py
+++ style.py
@@ -6,6 +6,6 @@ workbook = xlsxwriter.Workbook(r"../data entry spread sheet_new1.xlsx")
 worksheet = workbook.add_worksheet()
 
 worksheet.data_validation('H2', {'validate': 'list','source': ['complete', 'inprogress', 'not complete']})
-
+  
 
 workbook.close()
